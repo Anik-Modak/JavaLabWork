3b2f1ad1eb03f26f98b633e7d9fdfbec185422ab
Anik-Modak <anikmodak.rucse@gmail.com>
2018-09-24 12:39:00 +0600

    Added a new class MyRunnable

--- Multithreading/src/main/GreetingThreadRunner.java
+++ Multithreading/src/main/GreetingThreadRunner.java
@@ -9,8 +9,13 @@ public class GreetingThreadRunner
 		GreetingRunnable r2 = new GreetingRunnable("Fiaz");
 		GreetingRunnable r3 = new GreetingRunnable("Goodbye");
 		
-		r1.run();
-		r2.run();
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r2);
+		Thread t3 = new Thread(r3);
+		
+		t1.start();
+		t2.start();
+		t3.start();
 	}
 
 }
---
+++ Multithreading/src/main2/MyThreadRunner.java
@@ -0,0 +1,13 @@
+package main2;
+
+public class MyThreadRunner
+{
+
+	public static void main(String[] args) 
+	{
+		Thread t = new Thread(new MyRunnable());
+		t.start();
+		t.interrupt();
+	}
+
+}
