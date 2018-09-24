d76eaf1681835feea914a3174bef0a21212ff6dd
Anik-Modak <anikmodak.rucse@gmail.com>
2018-09-24 12:01:19 +0600

    OperatorTest using JunitTest

--- OperatorTest/src/main/OperatorClass.java
+++ OperatorTest/src/main/OperatorClass.java
@@ -5,69 +5,61 @@ public class OperatorClass
 
 	public int addition(int i, int j)
 	{
-		// TODO Auto-generated method stub
 		return i+j;
 	}
 
 	public int multiply(int i, int j)
 	{
-		// TODO Auto-generated method stub
 		return i*j;
 	}
 
 	public int division(int i, int j)
 	{
-		// TODO Auto-generated method stub
 		return i/j;
 	}
 
 	public int subtraction(int i, int j) 
 	{
-		// TODO Auto-generated method stub
 		return i-j;
 	}
 
 	public double d_division(double i, double j)
 	{
-		// TODO Auto-generated method stub
 		return i/j;
 	}
 
 	public boolean equals(int i, int j) 
 	{
-		// TODO Auto-generated method stub
 		return i == j? true:false;
 	}
 
 	public int leftshift(int i, int j) 
 	{
-		// TODO Auto-generated method stub
 		return i<<j;
 	}
 
 	public int rightshift(int i, int j)
 	{
-		// TODO Auto-generated method stub
 		return i>>j;
 	}
 
-	public int xor(int i, int j) {
-		// TODO Auto-generated method stub
+	public int xor(int i, int j) 
+	{
 		return i^j;
 	}
 
-	public int or(int i, int j) {
-		// TODO Auto-generated method stub
-		return 7|2;
+	public int or(int i, int j) 
+	{
+		return i|j;
 	}
 
-	public int and(int i, int j) {
-		// TODO Auto-generated method stub
-		return 7&2;
+	public int and(int i, int j) 
+	{
+		return i&j;
 	}
 
-	public int not(int i) {
-		// TODO Auto-generated method stub
+	public int not(int i) 
+	{
 		return ~i;
 	}
 
