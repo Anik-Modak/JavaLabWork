387aefd732db72711ade042796715ba0530ca00f
Anik-Modak <anikmodak.rucse@gmail.com>
2018-10-07 16:19:25 +0600

    Practice exercises E19.4

--- StreamProcessing/src/PracticeExercises4/MainClassE19_4.java
+++ StreamProcessing/src/PracticeExercises4/MainClassE19_4.java
@@ -3,12 +3,12 @@ package PracticeExercises4;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class MainClassE19_4 
 {
-
 	public static void main(String[] args) throws IOException
 	{
 		
@@ -20,11 +20,12 @@ public class MainClassE19_4
 		{ 
 			wordList.add(in.next());
 		}
-
+		
+		Collections.sort(wordList);
+		
 		wordList.stream()
 			.filter(w -> w.length()<=4)
 			.distinct().forEach(System.out :: println);
 
 	}
-
 }
