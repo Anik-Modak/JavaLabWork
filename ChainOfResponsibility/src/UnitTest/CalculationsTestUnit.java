f6cfb33903584d89fb4d284dc61bdb66dcb8ed60
Anik-Modak <anikmodak.rucse@gmail.com>
2018-11-11 16:17:07 +0600

    design pattern: Chain of Responsibility.using JUnitTest

--- ChainOfResponsibility/src/UnitTest/CalculationsTestUnit.java
+++ ChainOfResponsibility/src/UnitTest/CalculationsTestUnit.java
@@ -11,20 +11,56 @@ import ChainPattern.SubNumbers;
 
 class CalculationsTestUnit
 {
-	@Test
-	void test()
+	
+	Chain chain1 = new AddNumbers();
+	Chain chain2 = new SubNumbers();
+	Chain chain3 = new MultiplyNumbers();
+	Chain chain4 = new DivNumbers();
+	
+	void Setup()
 	{
-		Chain chain1 = new AddNumbers();
-		Chain chain2 = new SubNumbers();
-		Chain chain3 = new MultiplyNumbers();
-		Chain chain4 = new DivNumbers();
-
 		chain1.setNextChain(chain2);
 		chain2.setNextChain(chain3);
 		chain3.setNextChain(chain4);
-		
+	}
+	
+	@Test
+	void testAddition()
+	{
+		Setup();
 		Numbers request = new Numbers(10, 5, "add");
 		assertEquals(15, chain1.calculate(request), " ");
-
+	}
+	
+	@Test
+	void testSubtraction()
+	{
+		Setup();
+		Numbers request = new Numbers(10, 5, "sub");
+		assertEquals(5, chain1.calculate(request), " ");
+	}
+	
+	@Test
+	void testMultiplicationNumbers()
+	{
+		Setup();
+		Numbers request = new Numbers(10, 5, "multiply");
+		assertEquals(50, chain1.calculate(request), " ");
+	}
+	
+	@Test
+	void testDivision()
+	{
+		Setup();
+		Numbers request = new Numbers(10, 5, "div");
+		assertEquals(2, chain1.calculate(request), " ");
+	}
+	
+	@Test
+	void testOthers()
+	{
+		Setup();
+		Numbers request = new Numbers(10, 5, "pow");
+		assertEquals(-1, chain1.calculate(request), " ");
 	}
 }
